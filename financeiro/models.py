1a718a6457fa3bab6f725adbca3dbdca1e83d427
Andre <andre.amorim.pereira@gmail.com>
2015-04-10 17:33:09 -0300

    Adicionada classe de checkIn e codigo comentado

--- financeiro/models.py
+++ financeiro/models.py
@@ -70,9 +70,9 @@ class Cliente(models.Model):
 		return self.nome + "_" + str(self.id) + "_" + self.vencimento.strftime('%d/%m/%y')
 
 
-#####################################
-# Classes Externas - via WebService #
-#####################################
+############################################################
+#             Classes Externas - via WebService            #
+############################################################
 
 # Objetos serao instanciados atraves da comunicacao com o modulo "GITT Hotel"
 class CheckoutPayment(models.Model):
@@ -116,6 +116,10 @@ class Reservation(models.Model)	:
 
 
 # Objetos serao instanciados atraves da comunicacao com o modulo "PEDREIRO HOTELS"
+# Receberemos uma lista com varias triplas: descricao do produto, valor, quantidade
+# Se a conta for aprovada/paga, deve ser instaciada uma conta a pagar
+# Deve ser enviada uma mensagem para o modulo "Pedreiro" informando a mudança de status
+# Ou podemos simplesmente ignorar o status, e facilitar essa parte da logica
 class StockRequest(models.Model):
 
 	PAGO = 'PAGO'
@@ -131,14 +135,40 @@ class StockRequest(models.Model):
 	status = models.CharField(choices=TYPES, max_length=50)
 	data_requisicao = models.DateTimeField(null=True, blank=True, default = None)
 
+	# Linkando com Conta
+	id_conta = models.ForeignKey(Conta, null=True, blank=True, default = None)
+
 	def __str__(self):
 		return self.data_requisicao + "_" + str(self.id) + "_" + self.status
 
+# Tripla recebida do modulo "Pedreiros"
+# deve estar ligada a uma StockRequest
 class ProductRequest(models.Model):
 
-	id_produto = models.IntegerField()
+	descricao = models.IntegerField()
 	valor = models.IntegerField()
 	quantidade = models.IntegerField()
 
+	# Linkando com StockRequest
+	id_stockRequest = models.ForeignKey(StockRequest, null=True, blank=True, default = None)
+
 	def __str__(self):
 		return self.id_produto + "_" + str(self.id) + "_" + self.quantidade
+
+
+# Informacao vira do modulo "GITT"
+# Caso ja exista reserva previa, basta atualizar o objeto conta do respectivo CPF
+# Caso nao exista reserva previa, deve ser instanciada uma nova conta a receber com essas informacoes
+class CheckInConfirmation(models.Model)
+	
+	nome = models.CharField(max_length=200)
+	cpf = models.CharField(max_length=200)
+	valor_estadia = models.IntegerField()	#não inclui a reserva, equivale aos 100%
+	data_check_in = models.DateTimeField(null=True, blank=True, default = None)
+	data_check_out = models.DateTimeField(null=True, blank=True, default = None)
+
+	# Linkando com Conta
+	id_conta = models.ForeignKey(Conta, null=True, blank=True, default = None)
+
+	def __str__(self):
+		return self.cpf + "_" + str(self.id)
